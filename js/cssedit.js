44eb8d39cea4dc766a96cb4318f9e63457452cc8
bleistivt <bleistivt@users.noreply.github.com>
2015-05-22 15:54:27 +0200

    lint

--- js/cssedit.js
+++ js/cssedit.js
@@ -1,64 +1,74 @@
-jQuery(function($) {
-	var editor = ace.edit('AceEditor');
-	var leave = false;
-	editor.setTheme('ace/theme/crimson_editor');
-	var initmode = $("#Form_Preprocessor option:selected").val();
-	if (initmode == '1') {
-		editor.getSession().setMode('ace/mode/less');
-	} else if (initmode == '2') {
-		editor.getSession().setMode('ace/mode/scss');
-	} else {
-		editor.getSession().setMode('ace/mode/css');
-	}
-	var css = document.getElementById('Form_Style').value;
-	editor.setValue(css, -1);
-	editor.focus();
-	if (localStorage.getItem('scrollposition')) {
-		try {
-			var sp = JSON.parse(localStorage.getItem('scrollposition'));
-			editor.moveCursorToPosition(sp.pos);
-			editor.getSession().setScrollTop(sp.scroll);
-		} catch (e) { }
-	}
-	$('#AceEditor').show();
-	$('#NoJsForm').hide();
-	$('.CSSeditPrev').show();
-	$('.CSSeditSave, .CSSeditPrev').on('click', function(e) {
-		e.preventDefault();
-		leave = true;
-		$('#Form_Style').val(editor.getValue());
-		if ($(this).is('.CSSeditPrev')) {
-			$('#PreviewToggle').val(true);
-		}
-		localStorage.setItem('scrollposition', JSON.stringify({
-			pos : editor.getCursorPosition(),
-			scroll : editor.getSession().getScrollTop()
-		}));
-		$('#Form_CSSedit').submit();
-	});
-	$('#Form_Preprocessor').change(function() {
-		var selectboxvalue = $("#Form_Preprocessor option:selected").val();
-		if (selectboxvalue == '1') {
-			editor.getSession().setMode('ace/mode/less');
-		} else if (selectboxvalue == '2') {
-			editor.getSession().setMode('ace/mode/scss');
-		} else {
-			editor.getSession().setMode('ace/mode/css');
-		}
-	});
-	$('.CSSrevision').click(function(e) {
-		e.preventDefault();
-		var time = $(this).text();
-		$.get($(this).attr('href'), function(data) {
-			if (confirm('Load ' + time +
-				' revision?\nAll unsaved changes will be lost.')) {
-				editor.setValue(data);
-			}
-		});
-	});
-	$(window).on('beforeunload', function() {
-		if (editor.getValue() != css && !leave) {
-			return 'Do you really want to leave? Your changes will be lost.';
-		}
-	});
+/*jslint browser: true, this: true */
+/*global jQuery, window, ace*/
+
+jQuery(function ($) {
+    'use strict';
+
+    var editor = ace.edit('AceEditor'),
+        leave = false,
+        initmode = $("#Form_Preprocessor option:selected").val(),
+        css = document.getElementById('Form_Style').value,
+        scroll,
+        setMode = function (mode) {
+            if (mode === '1') {
+                editor.getSession().setMode('ace/mode/less');
+            } else if (mode === '2') {
+                editor.getSession().setMode('ace/mode/scss');
+            } else {
+                editor.getSession().setMode('ace/mode/css');
+            }
+        };
+
+    setMode(initmode);
+    editor.setTheme('ace/theme/crimson_editor');
+    editor.setValue(css, -1);
+    editor.focus();
+
+    if (localStorage.getItem('scrollposition')) {
+        try {
+            scroll = JSON.parse(localStorage.getItem('scrollposition'));
+            editor.moveCursorToPosition(scroll.pos);
+            editor.getSession().setScrollTop(scroll.scroll);
+        } catch (ignore) {}
+    }
+
+    $('#AceEditor').show();
+    $('#NoJsForm').hide();
+    $('.CSSeditPrev').show();
+
+    $('.CSSeditSave, .CSSeditPrev').on('click', function (e) {
+        e.preventDefault();
+        leave = true;
+        $('#Form_Style').val(editor.getValue());
+        if ($(this).is('.CSSeditPrev')) {
+            $('#PreviewToggle').val(true);
+        }
+        localStorage.setItem('scrollposition', JSON.stringify({
+            pos: editor.getCursorPosition(),
+            scroll: editor.getSession().getScrollTop()
+        }));
+        $('#Form_CSSedit').submit();
+    });
+
+    $('#Form_Preprocessor').change(function () {
+        setMode($("#Form_Preprocessor option:selected").val());
+    });
+
+    $('.CSSrevision').click(function (e) {
+        e.preventDefault();
+        var time = $(this).text();
+        $.get($(this).attr('href'), function (data) {
+            var confirm = window.confirm('Load ' + time + ' revision?\nAll unsaved changes will be lost.');
+            if (confirm) {
+                editor.setValue(data);
+            }
+        });
+    });
+
+    $(window).on('beforeunload', function () {
+        if (editor.getValue() !== css && !leave) {
+            return 'Do you really want to leave? Your changes will be lost.';
+        }
+    });
+
 });
