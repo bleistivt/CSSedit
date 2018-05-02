92dba2888c9a9d1fc380eeb2bde09f9753985440
bleistivt <bleistivt@users.noreply.github.com>
2018-05-02 06:14:57 +0200

    update view to new dashboard style

--- views/cssedit.php
+++ views/cssedit.php
@@ -1,61 +1,49 @@
 <?php if (!defined('APPLICATION')) exit();
 
-echo wrap($this->title(), 'h1');
+echo heading($this->title());
 
 if ($this->data('revisions')) {
-    echo '<div class="Help Aside" style="border-top:none;"><h2>', t('Revisions'), '</h2><ul>';
+    $revisions = '<ul>';
     foreach ($this->data('revisions') as $time => $url) {
-        echo wrap(anchor(Gdn_Format::dateFull($time), $url, 'CSSrevision'), 'li');
+        $revisions .= wrap(anchor(Gdn_Format::dateFull($time), $url, 'CSSrevision'), 'li');
     }
-    echo '</ul></div>';
+    $revisions .= '</ul>';
+    helpAsset(t('Revisions'), $revisions);
 }
 ?>
 
-<div class="Configuration">
-<div class="ConfigurationForm">
+<div id="AceEditor" style="height:550px;display:none;margin:0 -1.125rem;border-bottom:0.0625rem solid #e7e8e9;"></div>
 
-<ul>
-    <li>
-        <div id="AceEditor" style="height:550px;border-bottom:1px solid #82bddd;display:none;"></div>
-    </li>
-</ul>
 <?php
 echo $this->Form->open(['id' => 'Form_CSSedit']);
 echo $this->Form->errors();
 ?>
 <ul>
-    <li id="NoJsForm">
+    <li class="form-group" id="NoJsForm">
     <?php echo $this->Form->textBox('Style', [
         'MultiLine' => true,
         'class' => 'InputBox WideInput'
     ]); ?>
     </li>
-    <li>
-    <?php echo $this->Form->dropDown('Preprocessor', [
-        0 => 'CSS',
-        1 => 'LESS',
-        2 => 'SCSS'
-    ]); ?>
-    <div style="display:inline-block;">
-        <?php echo $this->Form->checkBox('Mobile', 'Enable on mobile theme'); ?>
-    </div>
+    <li class="form-group">
+        <?php echo $this->Form->toggle('Mobile', 'Enable on mobile theme'); ?>
     </li>
-    <li>
-      <?php if ($this->Form->getValue('Style') && c('Garden.Theme') == 'default') {
-                // taget="_blank" until there is a way to download a file from a popup.
-                echo anchor(t('Export as theme'), '/settings/cssexport', ['target' => '_blank']);
-            } ?>
-    </li>
-    <li>
-    <?php echo $this->Form->hidden('Preview', ['id' => 'PreviewToggle', 'value' => false]);
-          echo $this->Form->button('Preview', [
-            'class' => 'Button CSSeditPrev',
-            'style' => 'display:none;'
-          ]);
-          echo $this->Form->button('Save', ['class' => 'Button CSSeditSave']); ?>
-    </li>
+    <?php if ($this->Form->getValue('Style') && c('Garden.Theme') == 'default') {
+        // taget="_blank" until there is a way to download a file from a popup.
+        echo wrap(
+            anchor(t('Export as theme'), '/settings/cssexport', ['target' => '_blank', 'class' => 'btn']),
+            'li',
+            ['class' => 'form-group']
+        );
+    } ?>
 </ul>
-<?php echo $this->Form->close(); ?>
+<div class="form-footer">
+<?php echo $this->Form->hidden('Preview', ['id' => 'PreviewToggle', 'value' => false]);
+      echo $this->Form->button('Preview', [
+        'class' => 'Button CSSeditPrev',
+        'style' => 'display:none;'
+      ]);
+      echo $this->Form->button('Save', ['class' => 'Button CSSeditSave']); ?>
+</div>
 
-</div>
-</div>
+<?php echo $this->Form->close(); ?>
