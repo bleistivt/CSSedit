7faa5174a5cc29f13cace3bad79aa9a6853e39b1
bleistivt <bleistivt@users.noreply.github.com>
2018-05-02 06:13:48 +0200

    v2.0
    - css cache is now served from /uploads directory due to .htaccess changes
    - removed preprocessors since they mostly aren't compatible with PHP7 anymore
    - turned export view into ConfigurationModule view
    - now exports theme info as addon.json (instead of about.php)
    - uses ace editor from CDN

--- class.cssedit.plugin.php
+++ class.cssedit.plugin.php
@@ -4,8 +4,10 @@ class CSSeditPlugin extends Gdn_Plugin {
 
     public function __construct() {
         parent::__construct();
-        $this->sourceDir = PATH_UPLOADS.'/CSSedit/source/';
-        $this->cacheDir = PATH_UPLOADS.'/CSSedit/cache/';
+        $this->sourcePath = '/CSSedit/source/';
+        $this->cachePath = '/CSSedit/cache/';
+        $this->sourceDir = PATH_UPLOADS.$this->sourcePath;
+        $this->cacheDir = PATH_UPLOADS.$this->cachePath;
     }
 
 
@@ -16,7 +18,7 @@ class CSSeditPlugin extends Gdn_Plugin {
         }
         if ($preview = Gdn::session()->stash('CSSeditPreview', '', false)) {
             // Preview a stylesheet
-            $sender->addCssFile(asset('cache/CSSedit/'.$preview, true));
+            $sender->addCssFile(Gdn_Upload::url($this->cachePath.$preview));
             $icon = smartAsset('plugins/CSSedit/icon.png');
             $sender->informMessage(
                 wrap('', 'span', [
@@ -28,13 +30,13 @@ class CSSeditPlugin extends Gdn_Plugin {
             );
         } elseif (c('CSSedit.Token')) {
             // Add the actual stylesheet to the page.
-            $sender->addCssFile(asset('cache/CSSedit/'.c('CSSedit.Token').'.css', true));
+            $sender->addCssFile(Gdn_Upload::url($this->cachePath.c('CSSedit.Token').'.css'));
         }
     }
 
 
     // Adds the CSSedit link to the Dashboard.
-    public function base_getAppSettingsMenuItems_handler($sender, &$args) {
+    public function base_getAppSettingsMenuItems_handler($sender, $args) {
         $args['SideMenu']->addLink('Appearance', t('CSS Editor'), 'settings/cssedit', 'Garden.Settings.Manage');
     }
 
@@ -49,12 +51,11 @@ class CSSeditPlugin extends Gdn_Plugin {
 
         if ($sender->Form->authenticatedPostBack()) {
             // Save the config values.
-            saveToConfig('CSSedit.Preprocessor', $sender->Form->getValue('Preprocessor', 0));
             saveToConfig('CSSedit.Mobile', $sender->Form->getValue('Mobile'));
 
             // Try to save the stylesheet.
             try {
-                $this->save($sender->Form->getValue('Style', ''), $sender->Form->getValue('Preprocessor'), $preview);
+                $this->save($sender->Form->getValue('Style', ''), $preview);
                 $sender->informMessage(sprite('Check', 'InformSprite').t('Your changes have been saved.'), 'HasSprite');
             } catch (Exception $e) {
                 $message = t('Compilation Error:').'<br>'.$e->getMessage();
@@ -65,7 +66,6 @@ class CSSeditPlugin extends Gdn_Plugin {
 
         } else {
             // Prepare the form.
-            $sender->Form->setValue('Preprocessor', c('CSSedit.Preprocessor'));
             $sender->Form->setValue('Mobile', c('CSSedit.Mobile'));
             Gdn::session()->stash('CSSeditPreview');
 
@@ -87,10 +87,15 @@ class CSSeditPlugin extends Gdn_Plugin {
         $sender->Form->setValue('Style', $source);
         $sender->title(t('CSS Editor'));
         $sender->setData('revisions', $this->revisions());
+        $sender->setHighlightRoute('settings/cssedit');
+        $sender->cssedit = true;
 
-        $sender->addSideMenu('settings/cssedit');
-        $sender->addJsFile('ace.js', 'plugins/CSSedit/js/ace-min-noconflict');
+        $sender->addJsFile('//cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/ace.js');
+        $sender->addJsFile('//cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/mode-css.js');
+        $sender->addJsFile('//cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/worker-css.js');
+        $sender->addJsFile('//cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/theme-crimson_editor.js');
         $sender->addJsFile('cssedit.js', 'plugins/CSSedit');
+
         $sender->addDefinition('CSSedit.loadMessage', t("Load %s revision?\nAll unsaved changes will be lost."));
         $sender->addDefinition('CSSedit.leaveMessage', t('Do you really want to leave? Your changes will be lost.'));
 
@@ -101,37 +106,41 @@ class CSSeditPlugin extends Gdn_Plugin {
     // Download the style as a theme.
     public function settingsController_cssExport_create($sender) {
         $sender->permission('Garden.Settings.Manage');
+        $sender->setHighlightRoute();
+        $sender->title(t('Export as theme'));
+        $sender->setData(
+            'Description',
+            t('This packages your stylesheet so that it can be installed like a regular theme. <strong>If you have made changes, save before using this.</strong>')
+        );
+
         // $ThemeInfo array
         $default = [
-            'Name' => t('Untitled'),
-            'Version' => '1.0',
-            'Author' => Gdn::session()->User->Name,
-            'Description' => t('Created with CSSedit plugin, ').Gdn_Format::date()
+            'name' => t('Untitled'),
+            'version' => '1.0',
+            'authors' => Gdn::session()->User->Name,
+            'description' => t('Created using CSSedit plugin, ').Gdn_Format::date()
         ];
 
         if ($sender->Form->authenticatedPostBack()) {
             $post = array_filter(array_map('trim', $sender->Form->formValues()));
             // Merge with the defaults.
             $default = array_intersect_key($post, $default) + $default;
-            $slug = Gdn_Format::url($default['Name']);
+            $slug = Gdn_Format::url($default['name']);
 
-            // Write the contents of the about.php file.
-            $about = "<?php\n\n".'$ThemeInfo[\''.$slug.'\'] = '.var_export($default, true).";\n";
+            // Build addon.json
+            $addon = $default;
+            $addon['key'] = $slug;
+            $addon['type'] = 'theme';
+            $addon['authors'] = [['name' => $addon['authors']]];
 
             $zip = new ZipArchive();
             $file = tempnam(sys_get_temp_dir(), 'cssedit');
 
             if ($this->stylesheet() && $zip->open($file, ZIPARCHIVE::OVERWRITE) === true) {
                 // Add the stylesheet to the archive.
-                if (!c('CSSedit.Preprocessor')) {
-                    $zip->addFile($this->stylesheet(true), $slug.'/design/custom.css');
-                } else {
-                    // Add the source as a text file if a preprocessor is used.
-                    $zip->addFile($this->stylesheet(), $slug.'/design/custom.css');
-                    $zip->addFile($this->stylesheet(true), $slug.'/design/source.txt');
-                }
+                $zip->addFile($this->stylesheet(true), $slug.'/design/custom.css');
                 // Add the theme info file.
-                $zip->addFromString($slug.'/about.php', $about);
+                $zip->addFromString($slug.'/addon.json', json_encode($addon, JSON_PRETTY_PRINT));
                 $zip->close();
 
                 Gdn_FileSystem::serveFile($file, $slug.'.zip', 'application/zip');
@@ -144,9 +153,27 @@ class CSSeditPlugin extends Gdn_Plugin {
         }
         $sender->Form->setData($default);
 
-        $sender->addSideMenu();
-        $sender->title(t('Export as theme'));
-        $sender->render('export', '', 'plugins/CSSedit');
+        $conf = new ConfigurationModule($sender);
+        $conf->schema([
+            'name' => [
+                'Control' => 'textbox',
+                'LabelCode' => 'Name'
+            ],
+            'version' => [
+                'Control' => 'textbox',
+                'LabelCode' => 'Version'
+            ],
+            'authors' => [
+                'Control' => 'textbox',
+                'LabelCode' => 'Author'
+            ],
+            'description' => [
+                'Control' => 'textbox',
+                'LabelCode' => 'Description',
+                'Options' => ['MultiLine' => true]
+            ]
+        ]);
+        $conf->renderAll();
     }
 
 
@@ -162,8 +189,8 @@ class CSSeditPlugin extends Gdn_Plugin {
     }
 
 
-    // Compile and minify the stylesheet.
-    private function save($string, $preprocessor, $preview = false) {
+    // Minify and save the stylesheet.
+    private function save($string, $preview = false) {
         // Use the creation timestamp for the filename.
         $token = time();
 
@@ -179,9 +206,9 @@ class CSSeditPlugin extends Gdn_Plugin {
             Gdn_FileSystem::saveFile($this->cacheDir.'previewsrc.css', $string);
             Gdn::session()->stash('CSSeditPreviewSource', $this->cacheDir.'previewsrc.css');
 
-            $string = $this->process($string, $preprocessor);
+            $string = Minify_CSS_Compressor::process($string);
             Gdn_FileSystem::saveFile($this->cacheDir.$token.'.preview.css', $string);
-            
+
             // Stash the compiled style for the preview.
             Gdn::session()->stash('CSSeditPreview', $token.'.preview.css');
 
@@ -192,26 +219,13 @@ class CSSeditPlugin extends Gdn_Plugin {
             Gdn_FileSystem::saveFile($this->sourceDir.time().'.rev.css', $string);
             $this->clean();
 
-            $string = $this->process($string, $preprocessor);
+            $string = Minify_CSS_Compressor::process($string);
             Gdn_FileSystem::saveFile($this->cacheDir.$token.'.css', $string);
-            
+
             // Remove the old file and save the new token to the configuration.
             Gdn_FileSystem::removeFolder($this->stylesheet());
             saveToConfig('CSSedit.Token', $token);
         }
-    }
-
-
-    // Processes a CSS string. Compilation errors throw exceptions.
-    private function process($string, $preprocessor = 0) {
-        if ($preprocessor == 1) {
-            $less = new lessc();
-            $string = $less->compile($string);
-        } elseif ($preprocessor == 2) {
-            $scss = new scssc();
-            $string = $scss->compile($string);
-        }
-        return Minify_CSS_Compressor::process($string);
     }
 
 
@@ -229,7 +243,7 @@ class CSSeditPlugin extends Gdn_Plugin {
     private function revisions() {
         $revisions = [];
         foreach (array_reverse(glob($this->sourceDir.'*.rev.css')) as $rev) {
-            $revisions[basename($rev, '.rev.css')] = Gdn_Upload::url('CSSedit/'.basename($rev));
+            $revisions[basename($rev, '.rev.css')] = Gdn_Upload::url($this->sourcePath.basename($rev));
         }
         return $revisions;
     }
